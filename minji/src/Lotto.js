ced124e49012abf15312a82f17d42b68fa6cc2ce
mjmj0217 <mjmj0217@naver.com>
2023-08-08 22:14:42 +0900

    feat: 로또 클래스 추가

--- minji/src/Lotto.js
+++ minji/src/Lotto.js
@@ -1,18 +1,39 @@
+const { Validator } = require("./Validator");
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
-    this.validate(numbers);
-    this.#numbers = numbers;
+    Validator.validateNumber(numbers);
+
+    this.#numbers = numbers.split(",");
   }
 
-  validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+  setBonusNumber(number) {
+    Validator.validateBonusNumber(number);
+    this.#numbers.push(number);
+  }
+
+  getRank(numbers) {
+    const result = this.#returnMatchResult(numbers);
+
+    if (result.general < 3) return 0;
+    if (result.general === 3) return 5;
+    if (result.general === 4) return 4;
+    if (result.general === 5 && result.bonus === 1) return 2;
+    if (result.general === 6) return 1;
+    return result.general;
+  }
+
+  #returnMatchResult(numbers) {
+    const result = { general: 0, bonus: 0 };
+    for (let i = 0; i < 6; i += 1) {
+      if (numbers.includes(Number(this.#numbers[i]))) result.general++;
     }
-  }
+    if (numbers.includes(Number(this.#numbers[6]))) result.bonus++;
 
-  // TODO: 추가 기능 구현
+    return result;
+  }
 }
 
 module.exports = Lotto;
